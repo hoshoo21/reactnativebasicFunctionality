156eba42736e798ea25867c9736288b8e697bd70
hoshoo21 <c_gawera@yahoo.com>
2025-03-02 19:49:56 -0800

    box object properties

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import react from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ExerciseComp from './components/ExerciseComp';
+import ListScreen from './components/ListScreen';
+import HomeScreen from './components/Home';
+import ImageScreen from './components/ImageScreen';
+import CalcScreen from './components/CalcScreen';
+import ColorScreen from './components/ColorScreen';
+import SquareScreen from './components/SquareScreen';
+import TextScreen from './components/TextScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App =()=>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">  
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
+        <Stack.Screen name="Exercise" component={ExerciseComp} />
+        <Stack.Screen name="Images" component={ImageScreen} />
+        <Stack.Screen name="Counter" component={CalcScreen} />
+        <Stack.Screen name="Colors" component={ColorScreen} />
+        <Stack.Screen name="Square" component={SquareScreen} />
+        <Stack.Screen name="TextScreen" component={TextScreen} />
+     
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
